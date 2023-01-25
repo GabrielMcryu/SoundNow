180ab24ce10ae696aba7db85eb6ae654f8c90734
GabrielMcryu <gabrielmcryu@gmail.com>
2023-01-25 11:15:12 +0300

    Crated Music player UI

---
+++ src/js/config.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDmZJYVJu-PelccEAnB7pAjij9uzS0M7UA",
+  
+    authDomain: "sound-now.firebaseapp.com",
+  
+    projectId: "sound-now",
+  
+    storageBucket: "sound-now.appspot.com",
+  
+    messagingSenderId: "525464756177",
+  
+    appId: "1:525464756177:web:31f5dfc28eff48ca894af0"
+  
+  };
+
+  export default firebaseConfig;
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
+import firebaseConfig from './config.js'
+
+initializeApp(firebaseConfig);
+
+console.log("hello world");
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'test');
+
+// get collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        console.log(snapshot.docs);
+    });
